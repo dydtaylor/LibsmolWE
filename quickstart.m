9f5f40e40e68d2e5fb4b120f7aad7de7c28d40cd
Rob <robertbt@uci.edu>
2021-09-27 20:49:44 -0700

    Update quickstart.m
    flux vs iter graph

--- quickstart.m
+++ quickstart.m
@@ -21,3 +21,9 @@ MFPT = 1/meanFluxes;
 
 fprintf('The MFPT measured from the dataset is: %f \n The domain length is: %f \n The number of molecules in the domain is: %f \n The domain density is: %f \n The dynamics timestep (dt) is: %f \n The WE timestep (tau) is: %f (units of dt) \n', MFPT, DynParams(2), DynParams(8), DynParams(8)/(DynParams(2)^2), DynParams(1), WEParams(1))
 
+figure
+plot(fluxData)
+set(gca,'yscale','log')
+title('Flux measured at timestep')
+ylabel('WE Step')
+xlabel('Flux')
